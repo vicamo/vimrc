d19b8c75b3047f1589737d2a143b558ba2050f3c
Vicamo Yang <vyang@mozilla.com>
2012-04-23 23:12:34 +0800

    populate files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ vimrc
 =====
 
 My personal vimrc
+
+INSTALL
+-------
+wget -O - https://raw.github.com/vicamo/vimrc/master/install.sh | sh
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+VIMHOME=~/.vim
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+[ -e "$VIMHOME" ] && die "$VIMHOME already exists."
+[ -e ".vimrc" ] && die ".vimrc already exists."
+[ -e ".gvimrc" ] && die ".gvimrc already exists."
+
+git clone git://github.com/vicamo/vimrc.git "$VIMHOME"
+
+CWD=`pwd`
+cd "$VIMHOME"
+git submodule update --init
+cd "$CWD"
+
+ln -s "$VIMHOME/vimrc" .vimrc
+ln -s "$VIMHOME/gvimrc" .gvimrc
+
+echo "vimrc is installed."
+
